a686034ff59b6604ed44f124b22271223bf04401
Moudy <mood93@hotmail.com>
2017-03-08 00:44:10 -0600

    Polished Menu & Added music
    S6L123

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -8,9 +8,15 @@ public class MusicPlayer : MonoBehaviour {
 
     static MusicPlayer instance = null;
 
-    void Awake() { 
+    public AudioClip startClip;
+    public AudioClip gameClip;
+    public AudioClip endClip;
+
+    private AudioSource music;
+
+    void Start() { 
         Debug.Log ("Musis player Awake " + GetInstanceID());
-        if (instance != null)
+        if (instance != null && instance != this)
         {
             Destroy(gameObject);
             print("Duplicate music plater self-destructing!");
@@ -18,7 +24,32 @@ public class MusicPlayer : MonoBehaviour {
         else {
             instance = this;
             GameObject.DontDestroyOnLoad(gameObject);
+            music = GetComponent<AudioSource>();
+            music.clip = startClip;
+            music.loop = true;
+            music.Play();
         }
     }
 	
+    void OnLevelWasLoaded(int level) {
+        Debug.Log("MusicPlayer: loaded level" + level);
+        music.Stop();
+
+        if(level == 0)
+        {
+            music.clip = startClip;
+        }
+        if (level == 1)
+        {
+            music.clip = gameClip;
+        }
+        if (level == 2)
+        {
+            music.clip = endClip;
+        }
+
+        music.loop = true;
+        music.Play();
+    }
+
 }
